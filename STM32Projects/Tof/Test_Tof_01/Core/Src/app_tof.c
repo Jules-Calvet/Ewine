2e70696b92c878347f494992658b380c51c88159
justin <just1.nicols@isen.yncrea.fr>
2023-01-04 10:54:45 +0100

    add test tof + test ir

---
+++ STM32Projects/Tof/Test_Tof_01/Core/Src/app_tof.c
@@ -0,0 +1,157 @@
+/**
+  ******************************************************************************
+  * @file          : app_tof.c
+  * @author        : IMG SW Application Team
+  * @brief         : This file provides code for the configuration
+  *                  of the STMicroelectronics.X-CUBE-TOF1.3.1.0 instances.
+  ******************************************************************************
+  *
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "app_tof.h"
+#include "main.h"
+#include "stdio.h"
+#include "53l1a2_ranging_sensor.h"
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+#define TIMING_BUDGET (30U) /* 16 ms < TimingBudget < 500 ms */
+#define REPETITION (1500U) /* refresh rate for polling mode (milliseconds, shall be consistent with TimingBudget value) */
+
+/* Private variables ---------------------------------------------------------*/
+static RANGING_SENSOR_ProfileConfig_t ProfileCenter;
+static int32_t statusCenter = 0;
+
+/* Private function prototypes -----------------------------------------------*/
+static void VL_53L1A2_SimpleRanging_Init(void);
+static void VL_53L1A2_SimpleRanging_Process(void);
+static void print_result(RANGING_SENSOR_Result_t *Result);
+
+void VL_TOF_Init(void)
+{
+  /* USER CODE BEGIN SV */
+
+  /* USER CODE END SV */
+
+  /* USER CODE BEGIN TOF_Init_PreTreatment */
+
+  /* USER CODE END TOF_Init_PreTreatment */
+
+  /* Initialize the peripherals and the TOF components */
+
+  VL_53L1A2_SimpleRanging_Init();
+
+  /* USER CODE BEGIN TOF_Init_PostTreatment */
+
+  /* USER CODE END TOF_Init_PostTreatment */
+}
+
+/*
+ * LM background task
+ */
+void VL_TOF_Process(void)
+{
+  /* USER CODE BEGIN TOF_Process_PreTreatment */
+
+  /* USER CODE END TOF_Process_PreTreatment */
+
+  VL_53L1A2_SimpleRanging_Process();
+
+  /* USER CODE BEGIN TOF_Process_PostTreatment */
+
+  /* USER CODE END TOF_Process_PostTreatment */
+}
+
+static void VL_53L1A2_SimpleRanging_Init(void)
+{
+  // initialization of the center tof
+  statusCenter = VL53L1A2_RANGING_SENSOR_Init(VL53L1A2_DEV_CENTER);
+
+  // DEBUG ERROR
+  if (statusCenter != BSP_ERROR_NONE)
+  {
+    printf("VL53L1A2_RANGING_SENSOR_Init failed\n");
+    while(1);
+  }
+
+}
+
+static void VL_53L1A2_SimpleRanging_Process(void)
+{
+  uint32_t Id_Center;
+  RANGING_SENSOR_Result_t Result_Center;
+
+  // verify id of sensor
+  VL53L1A2_RANGING_SENSOR_ReadID(VL53L1A2_DEV_CENTER, &Id_Center);
+
+  // sensor configuration
+  ProfileCenter.RangingProfile = RS_MULTI_TARGET_SHORT_RANGE;
+  ProfileCenter.TimingBudget = TIMING_BUDGET;
+  ProfileCenter.Frequency = 0; // Induces intermeasurement period, set to ZERO for normal ranging
+  ProfileCenter.EnableAmbient = 0; // Enable: 1, Disable: 0
+  ProfileCenter.EnableSignal = 0; // Enable: 1, Disable: 0
+
+  // set the profile if different from default one
+  VL53L1A2_RANGING_SENSOR_ConfigProfile(VL53L1A2_DEV_CENTER, &ProfileCenter);
+
+  statusCenter = VL53L1A2_RANGING_SENSOR_Start(VL53L1A2_DEV_CENTER, RS_MODE_BLOCKING_CONTINUOUS);
+
+  if (statusCenter != BSP_ERROR_NONE)
+  {
+    printf("VL53L1A2_RANGING_SENSOR_Start_Center failed\n");
+    while(1);
+  }
+
+  while (1)
+  {
+    /* repetition mode */
+    statusCenter = VL53L1A2_RANGING_SENSOR_GetDistance(VL53L1A2_DEV_CENTER, &Result_Center);
+
+    if (statusCenter == BSP_ERROR_NONE)
+    {
+      printf("\n CENTER ");
+      print_result(&Result_Center);
+    }
+    HAL_Delay(REPETITION);
+
+  }
+}
+
+static void print_result(RANGING_SENSOR_Result_t *Result)
+{
+  uint8_t i, j;
+
+  for (i = 0; i < RANGING_SENSOR_MAX_NB_ZONES; i++)
+  {
+    printf("\nTargets = %lu", (unsigned long)Result->ZoneResult[i].NumberOfTargets);
+
+    for (j = 0; j < Result->ZoneResult[i].NumberOfTargets; j++)
+    {
+      printf("\n |---> ");
+      printf("Status = %ld, Distance = %5ld mm ",
+        (long)Result->ZoneResult[i].Status[j],
+        (long)Result->ZoneResult[i].Distance[j]);
+    }
+  }
+  printf ("\n");
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ STM32Projects/Tof/Test_Tof_01/Debug/Drivers/BSP/Components/vl53l1cb/modules/vl53l1_silicon_core.d
@@ -0,0 +1,42 @@
+Drivers/BSP/Components/vl53l1cb/modules/vl53l1_silicon_core.o: \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_silicon_core.c \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_ll_def.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_error_codes.h \
+ ../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_types.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_register_structs.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_register_map.h \
+ ../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform_user_config.h \
+ ../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform_user_defines.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_hist_structs.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_ll_device.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_dmax_structs.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_error_exceptions.h \
+ ../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_ll_def.h \
+ ../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform_log.h \
+ ../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform_user_data.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_def.h \
+ ../Drivers/BSP/Components/vl53l1cb/vl53l1cb.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_core.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_core_support.h \
+ ../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_silicon_core.h
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_ll_def.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_error_codes.h:
+../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_types.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_register_structs.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_register_map.h:
+../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform_user_config.h:
+../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform_user_defines.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_hist_structs.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_ll_device.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_dmax_structs.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_error_exceptions.h:
+../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_ll_def.h:
+../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform_log.h:
+../Drivers/BSP/Components/vl53l1cb/porting/vl53l1_platform_user_data.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_def.h:
+../Drivers/BSP/Components/vl53l1cb/vl53l1cb.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_core.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_core_support.h:
+../Drivers/BSP/Components/vl53l1cb/modules/vl53l1_silicon_core.h:
