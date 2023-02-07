6c0e8d14cb36ab61c8376dbb02b9acdf950080a1
Jules Calvet <jules.calvet@isen.yncrea.fr>
2023-02-07 16:49:00 +0100

    start mobile app loading page button darkmode / lightmode in settings

---
+++ Mobile_app/app/src/main/java/fr/isen/ewine/SettingsActivity.kt
@@ -0,0 +1,43 @@
+package fr.isen.ewine
+
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.DrawableRes
+import fr.isen.ewine.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySettingsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val sharedPref: SharedPreferences = getSharedPreferences("settings", 0)
+        var darkMode = sharedPref.getBoolean("dark_mode", false)
+
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mode(darkMode)
+
+        binding.buttonMode.setOnClickListener {
+            darkMode = !darkMode
+            mode(darkMode)
+            sharedPref.edit().putBoolean("dark_mode", darkMode).apply()
+        }
+    }
+
+    private fun mode(darkMode : Boolean) {
+        if (darkMode) {
+            binding.root.setBackgroundColor(Color.BLACK)
+            binding.buttonMode.setBackgroundResource(R.drawable.sun)
+            binding.buttonMode.foreground = getDrawable(R.drawable.sun)
+        } else {
+            binding.root.setBackgroundColor(Color.WHITE)
+            binding.buttonMode.setBackgroundResource(R.drawable.moon)
+            binding.buttonMode.foreground = getDrawable(R.drawable.moon)
+        }
+    }
+}
