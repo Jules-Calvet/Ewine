9cc29c10177a9a82b0ce7dd2a863aabf7f21dd1f
justin <just1.nicols@isen.yncrea.fr>
2023-03-22 17:13:43 +0100

    wifi : begin send data (not working yet)

--- STM32Projects/Wifi/Test_Wifi_04/Core/Src/wifi.c
+++ STM32Projects/Wifi/Test_Wifi_04/Core/Src/wifi.c
@@ -7,39 +7,70 @@
 
 
 char at[] = "\r\nAT\r\n";
-char version[] = "\r\nAT+GMR\r\n";
 char apConnect[] = "\r\nAT+CWJAP=\"Reseau du KGB\",\"12345678\"\r\n";
-char rxBuffer[100];
+char cwMode[] = "\r\nAT+CWMODE=1\r\n";
+char cipStart[] = "\r\nAT+CIPSTART=\"TCP\",\"https://tcpbin.com\",4242\r\n";
+char cipClose[] = "\r\nAT+CIPCLOSE\r\n";
+int rxBufferSize = 120;
+char rxBuffer[40];
+/*
+char ExtendBuffer(void);
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+char ExtendBuffer(void){
+	if (strlen(rxBuffer) == rxBufferSize) {
+	    // le tableau est plein
+		int newBufferSize = rxBufferSize * rxBufferSizeCount;
+		char *newBuffer[newBufferSize];
+		strcpy(rxBuffer, *newBuffer);
+		memset(rxBuffer, 0, sizeof(rxBuffer));
+		rxBufferSizeCount++;
+		return *newBuffer;
+	}
+	else { return rxBuffer; }
+}*/
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
   if (huart == &huart1)
   {
 	  //debug rxBuffer
-	  //printf("Wifi Response: %s\n", rxBuffer);
+	  printf("Wifi Response 1 : %s\n", rxBuffer);
 	  if(strstr(rxBuffer, "WIFI CONNECTE") != NULL)printf("Wifi_Init Successful !\n");
 	  else printf("Wifi_Init Failed !\n");
-	  memset(rxBuffer, 0, sizeof(rxBuffer));
 	  HAL_UART_Receive_IT(&huart1, (uint8_t*)rxBuffer, sizeof(rxBuffer)-1);
   }
 }
 
+void Wifi_Send_Data(char data[]){
+	//Start IP session
+	HAL_UART_Transmit(&huart1, (uint8_t*)cipStart, strlen(cipStart) , 1000);
+	printf("%s", cipStart);
+	//Start IP session
+	HAL_UART_Transmit(&huart1, (uint8_t*)cipClose, strlen(cipClose) , 1000);
+	printf("%s", cipClose);
+}
+
 void Wifi_Init(void){
+
 	//Put the wifi in listening mode
 	HAL_UART_Receive_IT(&huart1, (uint8_t*)rxBuffer, sizeof(rxBuffer)-1);
 	//check that the ESP8266 is operational
-	HAL_UART_Transmit(&huart1, (uint8_t*)at, strlen(at) , 1000); //Send
+	//HAL_UART_Transmit(&huart1, (uint8_t*)at, strlen(at) , 1000);
+	//printf("%s", at);
 
-	HAL_Delay(1000);
 	//connect the ESP8266 to 'Reseau du KGB' Wifi
-	HAL_UART_Transmit(&huart1, (uint8_t*)apConnect, strlen(apConnect) , 1000); //Connect
-	HAL_Delay(1000);
+	HAL_UART_Transmit(&huart1, (uint8_t*)apConnect, strlen(apConnect) , 1000);
+	printf("%s", apConnect);
+	//debug rxBuffer
+
+	//Set the Wi-Fi mode to station
+	HAL_UART_Transmit(&huart1, (uint8_t*)cwMode, strlen(cwMode) , 1000);
+	printf("%s", cwMode);
+
+	Wifi_Send_Data(cipStart);
 }
 
 void Wifi_Process(void){
-
-
+	HAL_Delay(1000);
 }
 
