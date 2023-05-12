d981baa01659d4af0738639d099937fd0bfc1a6d
Jules Calvet <jules.calvet@isen.yncrea.fr>
2023-05-12 10:51:11 +0200

    management display

--- Mobile_app/app/src/main/java/fr/isen/ewine/BottleActivity.kt
+++ Mobile_app/app/src/main/java/fr/isen/ewine/BottleActivity.kt
@@ -5,13 +5,20 @@ import android.content.SharedPreferences
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.gson.Gson
+import com.squareup.picasso.Picasso
 import fr.isen.ewine.databinding.ActivityBottleBinding
 import fr.isen.ewine.model.CellarData
 import fr.isen.ewine.model.UserData
 
 class BottleActivity : AppCompatActivity() {
     private lateinit var binding : ActivityBottleBinding
+
+    private lateinit var bottleTypeOfWine : String
+    private lateinit var bottleName : String
+    private lateinit var bottleProducerName : String
+    private lateinit var bottleYearOfProduction : String
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -35,9 +42,65 @@ class BottleActivity : AppCompatActivity() {
         val jsonFromPrefs = sharedPref.getString("tab_cellar", null)
         if(jsonFromPrefs != null) {
             val tabCellar = gson.fromJson(jsonFromPrefs, UserData::class.java)
-            binding.BottleTypeOfWine.text = tabCellar.cellarData[x+(y*sharedPref.getInt("width",0))].bottleTypeOfWine
+            bottleTypeOfWine = tabCellar.cellarData[x+(y*sharedPref.getInt("width",0))].bottleTypeOfWine
+            bottleName = tabCellar.cellarData[x+(y*sharedPref.getInt("width", 0))].bottleName
+            bottleProducerName = tabCellar.cellarData[x+(y*sharedPref.getInt("width", 0))].bottleProducerName
+            bottleYearOfProduction = tabCellar.cellarData[x+(y*sharedPref.getInt("width", 0))].bottleYearOfProduction
         }
         mode(darkMode)
+
+        displayManagement()
+    }
+
+    private fun displayManagement() {
+        if(bottleName.isNullOrEmpty()) {
+            runOnUiThread {
+                binding.BottleName.visibility = View.GONE
+            }
+        } else {
+            runOnUiThread {
+                binding.BottleName.text = bottleName
+            }
+        }
+        if(bottleTypeOfWine.isNullOrEmpty()) {
+            runOnUiThread {
+                binding.BottleTypeOfWine.visibility = View.GONE
+                binding.bottleColorImg.visibility = View.GONE
+            }
+        } else {
+            runOnUiThread {
+                binding.BottleTypeOfWine.text = bottleTypeOfWine
+                when (bottleTypeOfWine) {
+                    "Red", "Rouge" -> {
+                        Picasso.get().load(R.drawable.red).into(binding.bottleColorImg)
+                    }
+                    "White", "Blanc" -> {
+                        Picasso.get().load(R.drawable.white).into(binding.bottleColorImg)
+                    }
+                    "Rose", "Rosé" -> {
+                        Picasso.get().load(R.drawable.rose).into(binding.bottleColorImg)
+                    }
+                }
+            }
+        }
+        if(bottleProducerName.isNullOrEmpty()) {
+            runOnUiThread {
+                binding.BottleProducerName.visibility = View.GONE
+            }
+        } else {
+            runOnUiThread {
+                binding.BottleProducerName.text = bottleProducerName
+            }
+        }
+        if(bottleYearOfProduction.isNullOrEmpty()) {
+            runOnUiThread {
+                binding.BottleYearOfProduction.visibility = View.GONE
+            }
+        } else {
+            runOnUiThread {
+                binding.BottleYearOfProduction.text = bottleYearOfProduction
+            }
+        }
     }
     private fun mode(darkMode: Boolean) {
         if (darkMode) {
